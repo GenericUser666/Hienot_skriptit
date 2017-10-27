1b12fcf43ca7ef627ebb1018fabec57556f2c161
GenericUser666 <reksu2005@gmail.com>
2017-10-27 22:16:12 +0300

    When you put keyboard interup it exits and prints ruoka

--- ruoka.py
+++ ruoka.py
@@ -4,24 +4,30 @@ ruoka = ["Pizza", "Hamburger", "Salmon", "Ceasar salad", "Tacos"]
 
 def eparuoka ():
     satunnainen_numero = random.randint(0, len(ruoka) - 1)
-    return ruoka[satunnainen_numero] 
+    return ruoka[satunnainen_numero]
 
 print ("Do you want to know your least favorite food in 20 yrs. yes/no ")
 vastaus = raw_input()
 
 if vastaus == "yes":
-    print ("OK you have commited to answer four questions to determine what the asnwer will be.")
-    print(" ")
-    print ("1.Whats is your cuurent favorite food:")
-    lempiruoka = raw_input()
-    print ("2.What is your least favorite food currently")
-    juttu = raw_input()
-    print ("3.What would you order from McDonald's")
-    McDonald = raw_input()
-    print ("What do you think your least favorite food will be in 20 years")
-    moi = raw_input()
-    print( eparuoka() )
+    try :
+        print ("OK you have commited to answer four questions to determine what the answer will be.")
+        print(" ")
+        print ("1.Whats is your cuurent favorite food:")
+        lempiruoka = raw_input()
+        print ("2.What is your least favorite food currently")
+        juttu = raw_input()
+        print ("3.What would you order from McDonald's")
+        McDonald = raw_input()
+        print ("What do you think your least favorite food will be in 20 years")
+        moi = raw_input()
+        print (" ")
+    except KeyboardInterrupt:
+        print "You said yes"
+        print( eparuoka() )
+
+
 
 else:
-    print (": )")
+    print (": (")
     exit()
